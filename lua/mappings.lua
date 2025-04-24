f49e614771acf7e0a2af83694ab95d078c7da362
VPE <vpelle@ct2mc.com>
2025-04-24 18:45:32 +0200

    add git mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,27 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+
+require("gitsigns").setup{
+
+  on_attach = function(bufnr)
+
+    local gitsigns = require("gitsigns")
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Actions
+    map('n', '<leader>gs', gitsigns.stage_hunk, {desc= "gitsigns: stage_hunk"})
+    map('n', '<leader>gu', gitsigns.undo_stage_hunk, {desc= "gitsigns: undo stage hunk"})
+    map('n', '<leader>gr', gitsigns.reset_hunk, {desc= "gitsigns: reset hunk"})
+    map('v', '<leader>gs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, {desc= "gitsigns: stage hunk"})
+    map('v', '<leader>gr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, {desc= "gitsigns: reset hunk"})
+    map('n', '<leader>gp', gitsigns.preview_hunk_inline, {desc= "gitsigns: preview hunk inline"})
+
+  end
+}
