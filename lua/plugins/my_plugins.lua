d51e1c28f7d3d3c1618a253d31843f121296dfc2
vivipal <no@thank.you>
2025-05-20 09:07:39 +0200

    add json parser

--- lua/plugins/my_plugins.lua
+++ lua/plugins/my_plugins.lua
@@ -41,6 +41,14 @@ local plugins = {
       "tpope/vim-fugitive",
     },
   },
+  {
+    "VPavliashvili/json-nvim",
+    ft = "json", -- only load for json filetype
+    config = function()
+        vim.keymap.set("n", "<leader>jff", '<cmd>JsonFormatFile<cr>')
+        vim.keymap.set("n", "<leader>jmf", '<cmd>JsonMinifyFile<cr>')
+    end,
+  },
 
   -- {
   --  'VonHeikemen/fine-cmdline.nvim',
